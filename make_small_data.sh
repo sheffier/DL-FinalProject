224ff6e6bbc84ef6394101f756e6dc7389d5273b
erez sheffi <erez.sheffi@gmail.com>
2019-02-21 11:49:14 +0200

    fix data script

--- make_small_data.sh
+++ make_small_data.sh
@@ -1,10 +1,11 @@
 set -e
 
-SRC=train.box
-TRG=train.article
-N_LINES=10
+DATA_PATH=$PWD/data/processed_data/train
+SRC=$DATA_PATH/train.box
+TRG=$DATA_PATH/train.article
+N_LINES=100
 
-head -n $N_LINES $SRC.content > small.$SRC.content
-head -n $N_LINES $SRC.labels > small.$SRC.labels
-head -n $N_LINES $TRG.content > small.$TRG.content
-head -n $N_LINES $TRG.labels > small.$TRG.labels
+head -n $N_LINES $SRC.content > $SRC.small.content
+head -n $N_LINES $SRC.labels > $SRC.small.labels
+head -n $N_LINES $TRG.content > $TRG.small.content
+head -n $N_LINES $TRG.labels > $TRG.small.labels
